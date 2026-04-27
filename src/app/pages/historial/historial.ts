ed1cbf290251ecf48db604c38a6bbd570ec35362
Mrs-natiss <nathaliafanador13@gmail.com>
2026-04-26 22:25:29 -0500

    feat: rediseño completo segun mockup con paginacion

--- src/app/pages/historial/historial.ts
+++ src/app/pages/historial/historial.ts
@@ -11,8 +11,13 @@ import { PrecioService, Precio } from '../../services/precio';
 })
 export class HistorialComponent implements OnInit {
   precios: Precio[] = [];
+  preciosPaginados: Precio[] = [];
   cargando = true;
   error = false;
+  paginaActual = 1;
+  itemsPorPagina = 4;
+  totalPaginas = 1;
+  paginas: number[] = [];
 
   constructor(
     private precioService: PrecioService,
@@ -27,6 +32,9 @@ export class HistorialComponent implements OnInit {
     this.precioService.getPrecios().subscribe({
       next: (data) => {
         this.precios = data;
+        this.totalPaginas = Math.ceil(this.precios.length / this.itemsPorPagina);
+        this.paginas = Array.from({ length: this.totalPaginas }, (_, i) => i + 1);
+        this.actualizarPagina();
         this.cargando = false;
         this.cdr.detectChanges();
       },
@@ -38,12 +46,41 @@ export class HistorialComponent implements OnInit {
     });
   }
 
+  actualizarPagina() {
+    const inicio = (this.paginaActual - 1) * this.itemsPorPagina;
+    const fin = inicio + this.itemsPorPagina;
+    this.preciosPaginados = this.precios.slice(inicio, fin);
+    this.cdr.detectChanges();
+  }
+
+  irAPagina(pagina: number) {
+    this.paginaActual = pagina;
+    this.actualizarPagina();
+  }
+
+  paginaAnterior() {
+    if (this.paginaActual > 1) {
+      this.paginaActual--;
+      this.actualizarPagina();
+    }
+  }
+
+  paginaSiguiente() {
+    if (this.paginaActual < this.totalPaginas) {
+      this.paginaActual++;
+      this.actualizarPagina();
+    }
+  }
+
   eliminar(id: number) {
     if (confirm('¿Estás segura de que quieres eliminar este precio?')) {
       this.precioService.deletePrecio(id).subscribe({
         next: () => {
           this.precios = this.precios.filter(p => p.id !== id);
-          this.cdr.detectChanges();
+          this.totalPaginas = Math.ceil(this.precios.length / this.itemsPorPagina);
+          this.paginas = Array.from({ length: this.totalPaginas }, (_, i) => i + 1);
+          if (this.paginaActual > this.totalPaginas) this.paginaActual = this.totalPaginas;
+          this.actualizarPagina();
         },
         error: () => {
           alert('Error al eliminar el precio');
